b62ad2705028695d46b1d1fc83d4c0c961ff486c
christianD <christian.dimalanta1@gmail.com>
2025-08-18 09:39:46 +1000

    Changed it

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,9 @@
 import java.awt.Dimension;
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class Main extends JFrame {
     public static void main(String[] args) throws Exception {
@@ -18,10 +19,18 @@ public class Main extends JFrame {
       @Override
       public void paint(Graphics g) {
 	g.setColor(java.awt.Color.BLACK);
-	g.drawRect(10, 10, 700, 700);
-      }
-    }
 
+  //
+  
+  // Draws Grid
+  for(int i = 0; i < 10; i++) {
+    for(int j = 0; j < 10; j++) {
+      g.drawRect(10 + i * 70, 10 + j * 70, 70, 70);
+    }
+      }
+        }
+      }
+    
     private Main() {
       this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
       Canvas canvas = new Canvas();
