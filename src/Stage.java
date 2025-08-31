60f3e01999449b7806d9b0b5c360a7ac33c54e21
christianD <christian.dimalanta1@gmail.com>
2025-09-01 09:10:25 +1000

    Initial Commit - week05

--- src/Stage.java
+++ src/Stage.java
@@ -1,5 +1,7 @@
 import java.awt.Graphics;
 import java.awt.Point;
+import java.util.ArrayList;
+import java.util.List;  
 
 public class Stage {
   Grid grid;
@@ -12,6 +14,17 @@ public class Stage {
     cat = new Cat(grid.cellAtColRow(0, 0));
     dog = new Dog(grid.cellAtColRow(0, 15));
     bird = new Bird(grid.cellAtColRow(12, 9));
+
+    // Arraylists for Actors
+    List<actors extends Stage> actors = new ArrayList<>();
+    actors.add(cat);
+    actors.add(dog);
+    actors.add(bird);
+
+    for(Actor actor : actors) {
+      
+      System.out.println(actor);
+    }
   }
 
   public void paint(Graphics g, Point mouseLoc) {
